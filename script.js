d09cceeab29f93f3c1441d59c016af39b1799392
Ahsen Khan <akhan@SpeakerTech.local>
2024-07-05 12:41:30 -0400

    removed redundant redundancies

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { Scene, SphereGeometry, MeshStandardMaterial, Mesh, BoxGeometry, PerspectiveCamera, ACESFilmicToneMapping, WebGLRenderer } from 'three'
+import { Scene, PerspectiveCamera, ACESFilmicToneMapping, WebGLRenderer } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { WebGLPathTracer, GradientEquirectTexture } from 'three-gpu-pathtracer'
 
